7ef028e1da83e39aadcddd019dcc1f698a9fcecb
unknown <shafanafiazzahra@gmail.com>
2026-04-28 00:28:02 +0700

    fix error flake8

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
+import pandas as pd
+import os
+
 print("=== PROGRAM JALAN ===")
-import pandas as pd 
-import os 
 
-nilai_str = os.getenv('DATA_NILAI', '85,90,78,92,88') 
-nilai = list(map(int, nilai_str.split(','))) 
+nilai_str = os.getenv('DATA_NILAI', '85,90,78,92,88')
+nilai = list(map(int, nilai_str.split(',')))
 
-data = {'Nilai': nilai} 
-df = pd.DataFrame(data) 
+data = {'Nilai': nilai}
+df = pd.DataFrame(data)
 
-print('Analisis Data') 
-print(f"Rata-rata Nilai: {df['Nilai'].mean()}") 
-print('selesai') 
+print('Analisis Data')
+print(f"Rata-rata Nilai: {df['Nilai'].mean()}")
+print('selesai')
 
-#buat folder
+# Simpan CSV
 os.makedirs('/app/output', exist_ok=True)
+df.to_csv('/app/output/hasil.csv', index=False)
 
-#simpan csv 
-df.to_csv('/app/output/hasil.csv', index=False) 
-print("hasil disimpan") 
+print("hasil disimpan")
 print('selesai')
